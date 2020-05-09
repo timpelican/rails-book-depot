a5bb287825bde980363f4b835a1b2284c927872d
Tim Franklin <tim@pelican.org>
2020-05-09 20:06:16 +0100

    Add more validations.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
   def new_product(image_url)
     Product.new(title:        "My Book Title",
                 description:  "yyy",
@@ -46,4 +47,14 @@ class ProductTest < ActiveSupport::TestCase
         "#{image_url} shouldn't be valid"
     end
   end
+
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(title:        products(:ruby).title,
+                          description:  "yyy",
+                          price:        1,
+                          image_url:    "fred.gif")
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')],
+      product.errors[:title]
+    end
 end
