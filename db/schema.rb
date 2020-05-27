6cd6c91132279ef91dd0d33728f1fae26680693e
Tim Franklin <tim@pelican.org>
2020-05-27 10:46:54 +0100

    Upgrade to rails 6.0.3.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_28_090117) do
+ActiveRecord::Schema.define(version: 2020_05_27_094439) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
